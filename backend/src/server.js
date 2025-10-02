f8c9e7dc3f3723b3e26274779c5e6e3c58577dd4
PrateekRajput12 <ficredinn@gmail.com>
2025-10-02 22:26:27 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,13 +7,14 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-
+import { fileURLToPath } from "url";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(
   cors({
@@ -40,10 +41,12 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  // app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    // res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
