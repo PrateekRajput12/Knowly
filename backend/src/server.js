7f7b2b49b3b73337fabfb76cab6c8bc80553e785
PrateekRajput12 <ficredinn@gmail.com>
2025-10-03 16:34:35 +0530

    Link updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://knowly-8.onrender.com",
     credentials: true, // allow frontend to send cookies
   })
 );
